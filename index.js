d7dcc456b2e3fa0a0e9d1a5c492a81ccbe132bee
Alexis <omaroquendo221@gmail.com>
2020-09-01 18:26:55 -0500

    Se corrigio error en la respuesta del home

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
+const hoganMiddleware = require('hogan-middleware')
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'mustache')
-app.engine('mustache', require('hogan-middleware').__express)
+app.engine('mustache', hoganMiddleware.__express)
 
 app.get('/', (req, res, next) => {
     res.send('Hola Mundo')
