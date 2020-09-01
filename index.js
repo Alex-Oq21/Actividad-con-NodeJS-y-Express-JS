3f326f5b0a0b86700c66ae4c0cb658f16e2986be
Alexis <omaroquendo221@gmail.com>
2020-09-01 18:16:26 -0500

    Respuestas

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', require('hogan-middleware').__express)
+
 app.get('/', (req, res, next) => {
-    res.send('Hola Mundo');
+    res.send('Hola Mundo')
+})
+
+app.get('/json', (req, res, next) => {
+    const data = {
+        saludo: 'Hola a todos!'
+    }
+    res.json(data)
+})
+
+app.get('/home', (req, res, next) => {
+    res.render('home', null)
 })
 
 app.listen(3000)
